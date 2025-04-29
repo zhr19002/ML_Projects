f642df64fddc84efc14477fb263d279f2b26b661
zhr19002 <100546013+zhr19002@users.noreply.github.com>
2025-04-29 17:35:38 -0400

    Add files via upload

---
+++ RL_ALE/DQN_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J DQN_job
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --mem=128G
+#SBATCH -t 12:00:00
+#SBATCH -p general-gpu
+#SBATCH --gres=gpu:1
+#SBATCH -o DQN_output.log
+#SBATCH -e DQN_error.log
+
+source /home/zhr19002/anaconda3/etc/profile.d/conda.sh
+conda activate pytorch-sb3
+cd /home/zhr19002/ML_projects/RL_ALE/
+srun python DQN_train.py
